4a4759fe0f880b07acaaf9512b00e70b68cbea27
Nikita Ilyasov <rainburst@gmail.com>
2014-01-26 18:13:32 +0400

    added mirror fot piratebay

--- app/controllers/pirate_controller.rb
+++ app/controllers/pirate_controller.rb
@@ -3,23 +3,36 @@ require 'net/http'
 class PirateController < ApplicationController
 
   def download
-    pirate_bay_url = "http://thepiratebay.se/search/#{params[:series]} s#{params[:season].rjust(2,'0')}e#{params[:episode].rjust(2,'0')}/0/7/208"
-    @url = URI.parse URI.escape(pirate_bay_url)
-    redirect_to(load_page @url)
+    hosts = ["http://quluxingba.info", "http://thepiratebay.se"]
+    magnet = magnet(hosts[1],params[:series],params[:season],params[:episode])
+    @url = magnet
+    redirect_to magnet
+
+    rescue Timeout::Error => e
+      magnet = magnet(hosts[0],params[:series],params[:season],params[:episode])
+      redirect_to magnet
   end
 
   private
 
+  def magnet host, series, season, episode
+    path = "#{host}/search/#{series} s#{season.rjust(2,'0')}e#{episode.rjust(2,'0')}/0/7/208"
+    uri = URI.parse URI.escape(path)
+    load_page uri
+  end
+
   def load_page url
     req = Net::HTTP::Get.new(url.path)
+    
     res = Net::HTTP.start(url.host, url.port) {|http|
+      http.read_timeout = 40
       http.request(req)
     }
-    logger.debug { res.body }
+
+
+    logger.debug { url }
     doc = Nokogiri::HTML(res.body)
-    table = doc.css("div.detName")[0].parent.css('a')[1]['href']
-    
-    table
+    doc.css("div.detName")[0].parent.css('a')[1]['href']
   end
 
 end
